a962835f4037dc666fd40297bed67e9a28b2e772
Andreas Schmitz <schmitz@terrestris.de>
2021-03-25 15:18:56 +0100

    Add tests

---
+++ src/test/java/de/terrestris/shogun/migrator/MigratorTest.java
@@ -0,0 +1,26 @@
+package de.terrestris.shogun.migrator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.IOException;
+
+public class MigratorTest {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/1.json", "/2.json", "/3.json"})
+    public void testMigration(String file) throws IOException {
+        JsonNode node = mapper.readTree(MigratorTest.class.getResource(file));
+        byte[] bs = Migrator.migrateApplication(node, mapper);
+        byte[] expected = IOUtils.toByteArray(MigratorTest.class.getResource("/migrated" + file));
+
+        Assertions.assertArrayEquals(expected, bs);
+    }
+
+}
